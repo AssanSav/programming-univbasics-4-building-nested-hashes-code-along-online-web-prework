05fc6aa8fe50da4735c2cf4483b8fdd746b6503a
assane savadogo <assane.savadogo81@gmail.com>
2019-07-06 17:21:29 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,32 +2,32 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name of "Lord Montague"
+        :name of "Lord Montague",
         :age of "53"
         },
       :matriarch => {
-        :name of "Lady Montague"
+        :name of "Lady Montague",
         :age of "54"
         },
       :hero => {
-        :name of "Romeo"
-        :age of "15"
+        :name of "Romeo",
+        :age of "15",
         :status of "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        :name of "Lord Capulet"
+        :name of "Lord Capulet",
         :age of "50"
         },
       :matriarch => {
-        :name of "Lady Capulet" 
+        :name of "Lady Capulet", 
         :age of "51"
         },
       :heroine => {
-       :name of "Juliet"
-       :age of "15"
+       :name of "Juliet",
+       :age of "15",
        :status of "alive"
         },
       :heroine_friends => []
